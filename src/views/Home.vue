b8892a9b10d4f5682e1a8b3932e733ea8bca5e75
Torje Bøhren <torje94@gmail.com>
2021-12-06 09:41:42 +0100

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+	<Todo :title="'Shopping list'" />
+</template>
+
+<script>
+	import Todo from '../components/Todo.vue';
+
+	export default {
+		components: {
+			Todo,
+		},
+	};
+</script>
